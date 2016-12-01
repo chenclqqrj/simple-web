d16fb5c1a28f2a328442cdfc415de1841d832023
chenclqqrj@gmail.com <chenclqqrj@gmail.com>
2016-12-01 17:16:51 +0800

    处理中文乱码

--- src/main/java/config/CustomRESTContext.java
+++ src/main/java/config/CustomRESTContext.java
@@ -3,8 +3,14 @@ package config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -12,8 +18,13 @@ import org.springframework.web.servlet.config.annotation.*;
         includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = RestController.class) })
 public class CustomRESTContext extends WebMvcConfigurerAdapter {
 
-    public void addViewControllers(ViewControllerRegistry registry){
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        converters.add(new Utf8StringHttpMessageConverter());
     }
+
 
     @Override
     public void configureDefaultServletHandling(
